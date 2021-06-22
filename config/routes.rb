2c0cf42e9a63185a0727cca0e4c3da830b2f72ba
Jim <jim@Jims-MacBook-Pro.local>
2021-06-22 10:04:42 +0930

    8.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
+  root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,5 @@ ActiveRecord::Schema.define(version: 2021_06_18_050044) do
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
